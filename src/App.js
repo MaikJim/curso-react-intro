4ef49b1f42b7fd3b37a913db70f8c96e72a6a902
MaikJim <maikoljimenez427@gmail.com>
2023-08-08 06:20:46 -0400

    build: :construction: Construction the structure in Components of the project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './platzi.webp';
 import './App.css';
-
+import { TodoCounter } from './Components/TodoCounter';
+import { TodoSearch } from './Components/TodoSearch';
+import { TodoList } from './Components/TodoList';
+import { TodoItem } from './Components/TodoItem';
+import { CreateTodoButton } from './Components/CreateTodoButton';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edita el archivo <code>src/App.js</code> y guarda para recargar.
-        </p>
-        <a
-          className="App-link"
-          href="https://platzi.com/reactjs"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <TodoCounter />
+      <TodoSearch />
+
+      <TodoList>
+        <TodoItem />
+        <TodoItem />
+        <TodoItem />
+      </TodoList>
+
+      <CreateTodoButton />
     </div>
   );
 }
---
+++ src/Components/CreateTodoButton.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function CreateTodoButton() {
+  return (
+    <button>Agregar TODOS</button>
+  )
+}
+
+export {CreateTodoButton}
